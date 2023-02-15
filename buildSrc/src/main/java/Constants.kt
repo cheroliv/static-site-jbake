80a57d26a9b2df9e5f548a2ccc7c47456cb02cdd
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-02-15 20:39:58 +0100

    clean build.gradle.kts

--- buildSrc/src/main/java/Constants.kt
+++ buildSrc/src/main/java/Constants.kt
@@ -1,7 +1,4 @@
 object Constants {
-    const val TASK_PUSH_PAGES = "pushPages"
-    const val TASK_BAKE = "bake"
-    const val TASK_PUBLISH_SITE = "publishSite"
     const val origin: String = "origin"
     const val remote: String = "remote"
     const val CNAME = "CNAME"
